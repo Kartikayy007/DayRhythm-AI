17305ea7532fb38c7d6af4e85e2ec5937188aa08
kartikayy007 <kartikayy6969@gmail.com>
2025-10-18 16:29:12 +0530

    Initial Commit

---
+++ DayRhythm AI/DayRhythm_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DayRhythm_AIApp.swift
+//  DayRhythm AI
+//
+//  Created by kartikay on 18/10/25.
+//
+
+import SwiftUI
+
+@main
+struct DayRhythm_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
